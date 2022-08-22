fe45b0e9241e2291234abe003b23dd48bb22ac7a
sagardwivedi <Sagar.dw20@gmail.com>
2022-08-22 18:40:35 +0530

    Component Deployment Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,58 @@
+import ProductImageD from "./assests/images/image-product-desktop.jpg";
+import ProductImageM from "./assests/images/image-product-mobile.jpg";
+
+import CartIcon from "./assests/images/icon-cart.svg";
+
 const App = () => {
-  return <div>App</div>;
+  const GiveMessage = () => {
+    alert("Hello World");
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center bg-cream">
+      <div className="grid w-[90%] max-w-2xl grid-cols-1 overflow-hidden rounded-xl sm:grid-cols-2">
+        <img
+          src={ProductImageD}
+          className="hidden h-full w-full bg-contain sm:block"
+          alt="logo"
+        />
+        <img
+          src={ProductImageM}
+          className="h-full w-full bg-contain sm:hidden"
+          alt="logo"
+        />
+        <div className="sm:space-y-6 space-y-5 bg-white p-[23px] sm:p-8">
+          <p className="uppercase tracking-[0.3rem] text-DarkGrayishBlue">
+            perfume
+          </p>
+          <h2 className="font-Fra text-3xl font-bold text-VeyDarkBlue sm:text-4xl">
+            Gabrielle Essence Eau De Parfum
+          </h2>
+          <p className="font-Mon text-sm font-medium text-DarkGrayishBlue">
+            A floral, solar and voluptous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL
+          </p>
+          <p className="flex flex-row items-center space-x-5">
+            <span className="font-Fra text-3xl font-bold text-DarkCyan">
+              $149.99
+            </span>
+            <span className="text-sm text-DarkGrayishBlue">
+              <del>$169.99</del>
+            </span>
+          </p>
+          <div className="items-end">
+            <button
+              onClick={GiveMessage}
+              className="flex w-full items-center justify-center space-x-2 rounded-xl bg-DarkCyan p-4 font-medium text-white transition-colors duration-300 ease-in-out hover:bg-VeyDarkBlue"
+            >
+              <img src={CartIcon} alt="" />
+              <span>Add to cart</span>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      cream: "#f2ebe3",
+      white: "#fff",
+      DarkCyan: "#3c8067",
+      VeyDarkBlue: "#1c232b",
+      DarkGrayishBlue: "#6c7289",
+    },
+    fontFamily: {
+      Fra: ["Fraunces", "serif"],
+      Mon: ["Montserrat", "sans-serif"],
+    },
   },
+
   plugins: [],
 };
