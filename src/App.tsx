65821ab2567c1ad5f7af2de3c8ad0ce50312ea11
sagardwivedi <Sagar.dw20@gmail.com>
2022-08-23 02:03:38 +0530

    Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import ProductImageD from "./assests/images/image-product-desktop.jpg";
-import ProductImageM from "./assests/images/image-product-mobile.jpg";
-import CartIcon from "./assests/images/icon-cart.svg";
+
+import { Images } from "./assests/images";
 
 const App = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-cream">
       <div className="grid w-[90%] max-w-2xl grid-cols-1 overflow-hidden rounded-xl sm:grid-cols-2">
-        <img
-          loading="eager"
-          src={ProductImageM}
-          className="h-full w-max bg-contain sm:hidden"
-          alt="logo"
-        />
-        <img
-          src={ProductImageD}
-          className="hidden h-full w-max bg-contain sm:block"
-          alt="logo"
-        />
+        <picture className="hidden sm:block">
+          <source className="w-full h-full" srcSet={Images.ProductImageWebPD} type="image/webp" />
+          <source className="w-full h-full" srcSet={Images.ProductImageD} type="image/jpeg" />
+          <img className="w-full h-full" src={Images.ProductImageD} alt="Product" />
+        </picture>
+
+        <picture className="sm:hidden">
+          <source className="w-full h-full" srcSet={Images.ProductImageWebM} type="image/webp" />
+          <source className="w-full h-full" srcSet={Images.ProductImageM} type="image/jpeg" />
+          <img className="w-full h-full" src={Images.ProductImageM} alt="Product" />
+        </picture>
+
         <div className="space-y-5 bg-white p-[23px] sm:space-y-6 sm:p-8">
           <p className="font-Mon uppercase tracking-[0.3rem] text-DarkGrayishBlue">
             perfume
@@ -32,14 +32,14 @@ const App = () => {
             <span className="font-Fra text-3xl font-bold text-DarkCyan">
               $149.99
             </span>
-            <span className="text-sm text-DarkGrayishBlue">
+            <p className="text-sm text-DarkGrayishBlue">
               <del>$169.99</del>
-            </span>
+            </p>
           </p>
           <div className="pt-4">
             <button className="flex w-full items-center justify-center space-x-2 rounded-xl bg-DarkCyan p-4 font-medium text-white ring-VeyDarkBlue ring-offset-1 hover:bg-VeyDarkBlue focus:ring-2">
-              <img className="h-min w-min" src={CartIcon} alt="cart" />
-              <span>Add to cart</span>
+              <img className="h-min w-min" src={Images.CartIcon} alt="cart" />
+              <p>Add to cart</p>
             </button>
           </div>
         </div>
---
+++ src/assests/images/index.ts
@@ -0,0 +1,14 @@
+import ProductImageD from "./image-product-desktop.jpg";
+import ProductImageWebPD from "./image-product-desktop.webp";
+import ProductImageM from "./image-product-mobile.jpg";
+import ProductImageWebM from "./image-product-mobile.webp";
+import CartIcon from "./icon-cart.svg";
+
+export const Images = {
+    ProductImageD,
+    ProductImageM,
+    ProductImageWebM,
+    ProductImageWebPD,
+    CartIcon
+}
+
